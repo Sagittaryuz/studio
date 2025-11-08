2ae8650656a7d35e328c8db5c22efb80add6d6b9
Marcos Rodrigues Pires <mrpires72@gmail.com>
2025-11-08 13:55:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2024049853-7fbf4",
+  "appId": "1:344238091063:web:2da84bdafa15fffe7388b2",
+  "apiKey": "AIzaSyB1ZqFqXC2tS0Ht5ncDHYnyaAY7HRSX-ms",
+  "authDomain": "studio-2024049853-7fbf4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "344238091063"
+};
