9f679361c82adba74a3ec96f0adede8bcba88883
Marcos Rodrigues Pires <mrpires72@gmail.com>
2025-11-08 15:08:00 +0000

    Mude o nome para Plano de Manutenção de Frota (_for element h1_)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Toaster } from '@/components/ui/toaster';
 import { FirebaseClientProvider } from '@/firebase';
 
 export const metadata: Metadata = {
-  title: 'JC Fleet Control',
+  title: 'Plano de Manutenção de Frota',
   description: 'Gestão de Manutenções Preventivas de Frotas',
 };
 
--- src/components/layout/app-header.tsx
+++ src/components/layout/app-header.tsx
@@ -18,7 +18,7 @@ export function AppHeader() {
       <div className="flex items-center gap-2">
         <JCIcon className="h-6 w-6 text-primary" />
         <h1 className="text-lg font-bold tracking-tight text-primary sm:text-xl">
-          JC Fleet Control
+          Plano de Manutenção de Frota
         </h1>
       </div>
       <div className="ml-auto flex items-center gap-4">
