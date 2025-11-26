1cdbb2db88d8f84eb9e689bcd38dbfdbc105534a
Marcos Rodrigues Pires <mrpires72@gmail.com>
2025-11-26 18:42:38 +0000

    Try fixing this error: `Runtime Error: Hydration failed because the serv

--- src/components/layout/app-header.tsx
+++ src/components/layout/app-header.tsx
@@ -17,7 +17,6 @@ export function AppHeader({ children }: { children?: React.ReactNode }) {
   return (
     <header className="sticky top-0 z-30 flex h-12 items-center gap-4 border-b bg-background px-4 sm:px-6">
       <Link href="/" className="flex items-center gap-2">
-        {children}
         <JCIcon className="h-6 w-6 text-primary" />
         <h1 className="text-lg font-bold tracking-tight text-primary sm:text-xl">
           Plano de Manutenção de Frota
