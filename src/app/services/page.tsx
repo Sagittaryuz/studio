5cc7e29887fc7680e14b48bb508d5db2b6aebf6f
Marcos Rodrigues Pires <mrpires72@gmail.com>
2025-11-29 14:56:45 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,6 +1,22 @@
 
 import { getDashboardData } from '@/lib/data';
 import { ServiceManagementClient } from './service-management-client';
+import dynamic from 'next/dynamic';
+import { Skeleton } from '@/components/ui/skeleton';
+
+const ServiceManagementClientWithNoSSR = dynamic(
+  () => import('./service-management-client').then(mod => mod.ServiceManagementClient),
+  { 
+    ssr: false,
+    loading: () => (
+        <div className="space-y-4">
+            <Skeleton className="h-10 w-1/3" />
+            <Skeleton className="h-20 w-full" />
+            <Skeleton className="h-40 w-full" />
+        </div>
+    )
+  }
+);
 
 
 export default async function ServiceManagementPage() {
@@ -9,7 +25,7 @@ export default async function ServiceManagementPage() {
   return (
     <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">
         <div className="max-w-6xl mx-auto">
-            <ServiceManagementClient 
+            <ServiceManagementClientWithNoSSR
                 initialServices={data.services} 
                 categories={data.categories}
             />
