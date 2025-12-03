8101010cf8e780926a05de995f4b3c58d8bc10ff
Marcos Rodrigues Pires <mrpires72@gmail.com>
2025-12-03 12:29:58 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
   "projectId": "studio-2024049853-7fbf4",
   "appId": "1:344238091063:web:2da84bdafa15fffe7388b2",
+  "storageBucket": "studio-2024049853-7fbf4.appspot.com",
   "apiKey": "AIzaSyB1ZqFqXC2tS0Ht5ncDHYnyaAY7HRSX-ms",
   "authDomain": "studio-2024049853-7fbf4.firebaseapp.com",
-  "measurementId": "",
+  "measurementId": "G-D4T13233CH",
   "messagingSenderId": "344238091063"
 };
